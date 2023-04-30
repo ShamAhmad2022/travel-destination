1176732218373a4cb09aed473e76974349102c13
ShamAhmad2022 <shamahmadaljalam@gmail.com>
2023-04-30 17:48:28 +0000

    lab11

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer class="idexfooter">
+        <div class="footerFlex">
+            <div class="SMFlex">
+                <p>Follow me on:</p>
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="#"><i class="fa-brands fa-github"></i></a>
+            </div>
+
+            <div class="footerEmail">
+                <p>Sham AL-Jalam</p>
+            </div>
+        </div>
+
+    </footer>
+    );
+}
+
+export default Footer;
